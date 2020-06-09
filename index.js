6fac2cf905501861afd5170b55dad3d3b064f817
Kate Sowles <hello@katesowles.com>
2020-06-09 10:49:54 -0700

    WIP added general helpers for string cleanup, date reformatting, and making object keys easier to read/parse

--- index.js
+++ index.js
@@ -5,6 +5,33 @@ const copy = require("copy-paste").copy;
 const datetime = require("luxon").DateTime;
 const indentString = require("indent-string");
 
+////////////////////////////////////////////////////////////////////////////////
+// general helpers /////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+const cleanupStrings = (value, fallback = "") => {
+  return String(value).trim() || fallback;
+};
+
+const reformatDates = (ttDateTime, ttTimezone) => {
+  if (!ttDateTime || !datetime.fromISO(ttDateTime).isValid) return null;
+  // NOTE: TickTick is providing timestamps in UTC so we want to be explicit
+  const parsedToUTC = datetime.fromISO(ttDateTime).toUTC();
+  // NOTE: We can set the zone on that newly-parsed time based on the zone
+  // coming from TickTick. If zone is invalid result falls back to .local() zone
+  const utcToLocal = parsedToUTC.setZone(ttTimezone);
+
+  return utcToLocal.toFormat("MM/dd/yyyy h:mm:ss a");
+};
+
+const keyToCamelCase = (key) => {
+  return key
+    .toLowerCase()
+    .split(" ")
+    .map((w, i) => (!!i ? w.charAt(0).toUpperCase() + w.slice(1) : w))
+    .join("");
+};
+
 const toContext = (text) => {
     if (/#[\d\w.-]+/.test(text)) {
         return text.replace(/#([\d\w.-]+)/g, "@context($1)");
